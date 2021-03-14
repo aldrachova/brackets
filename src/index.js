8e00aa7beebd66ead08c5e73577a5ae0c044f7c1
aldrachova <51236400+aldrachova@users.noreply.github.com>
2021-03-14 16:53:56 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0){
+        return false;
+    }
+    let bracketsPair = '';
+    let bracketsPairs = [];
+    for (let i = 0; i < bracketsConfig.length; i++){
+        bracketsPair = '';
+        for (let j = 0; j < bracketsConfig[i].length; j++){
+            bracketsPair = bracketsPair + bracketsConfig[i][j];
+        }
+        bracketsPairs.push(bracketsPair);
+    }
+    for (let i = 0; i < bracketsPairs.length; i++){
+        if (str.includes(bracketsPairs[i])){
+            str = str.replace(bracketsPairs[i], '');
+            i = -1;
+        }      
+    }
+    return str.length === 0;  
 }
